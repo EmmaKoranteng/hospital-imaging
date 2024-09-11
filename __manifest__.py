f4267169ade38c53cf0e6a95a98b2adedfbb57e0
emma <ekoranteng0705@gmail.com>
2024-09-11 06:37:05 +0000

    HOSPITAL IMAGING

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+
+{
+    'name': 'HMS Imaging',
+    'version': '1.0.0.0',
+    'author': 'gpha_it',
+    'maintainer': 'Apps unit.',
+    'complexity': 'easy',
+    'depends': [],
+    "license": "AGPL-3",
+    'category': 'Health',
+    'description': '''
+      A Module for Imaging.
+    ''',
+    'images': [],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+
+}
